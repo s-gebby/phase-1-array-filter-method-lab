f03743696d3b3a72d47569e071b4e98669dec1a2
silas <silasgebhart12@gmail.com>
2024-02-25 14:55:36 -0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+// Function to find drivers whose name exactly matches the given name
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+// Function to find drivers whose name starts with the provided letters
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+  );
+}
+
+// Function to find drivers whose name matches the provided name
+function matchName(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.name.toLowerCase() === name.toLowerCase()
+  );
+}
